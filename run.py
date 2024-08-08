920c7e32e31769c84ea7614730646734c72fef8a
D-Claffey-Code-Institute Claffey <d7x359i6@students.codeinstitute.net>
2024-08-08 11:04:14 +0000

    add for loop to simulate dice rolls

--- run.py
+++ run.py
@@ -14,4 +14,12 @@ def rand_ability_scores():
         "Wisdom": 0,
         "Charisma": 0
     }
+    for i in range(6):
+    # creates a list scores, generating a number between 1 and 6, 3 times
+    # sums the results and assigns them to each ability in the abilities dictionary
+        scores = [random.randint(1, 6) for x in range(3)]
+        abilities[list(abilities.keys())[i]] = sum(scores)
+    return abilities
 
+result = rand_ability_scores()
+print(result)
