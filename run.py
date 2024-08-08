d325d5aa5ecb3ca1cd3690f13d0f6522b9795dd0
D-Claffey-Code-Institute Claffey <d7x359i6@students.codeinstitute.net>
2024-08-08 10:52:22 +0000

    created a function and dictionary for abilities

--- run.py
+++ run.py
@@ -1,2 +1,17 @@
 import random
 
+def rand_ability_scores():
+    """
+    Generates six random ability scores simulating rolling 3 six sided dice
+    for each ability: Strenght, Dexterity, Constitution, Intelligence, Wisdom
+    and Charisma. 
+    """
+    abilities = {
+        "Strenght": 0,
+        "Dexterity": 0,
+        "Constitution": 0,
+        "Intelligence": 0,
+        "Wisdom": 0,
+        "Charisma": 0
+    }
+
